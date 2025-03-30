312c68c8cea179c38de27426374a37c7d7970ec1
Somerset-Wilsher <143337146+Somerset-Wilsher@users.noreply.github.com>
2025-03-30 21:45:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,6 @@ pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
-# Convert the Fruit_Name column to a list for use in st.multiselect
-fruit_list = pd_df['FRUIT_NAME'].tolist()
-
 ingredients_list = st.multiselect (
 'Choose up to 5 ingredients:'
 , my_dataframe
@@ -40,7 +37,7 @@ if ingredients_list:
        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
        st. subheader(fruit_chosen + ' Nutrition Information' )
-       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ search_on)
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
@@ -59,5 +56,5 @@ if ingredients_list:
    for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ''
         st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
